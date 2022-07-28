f82c557d5bd2b962f154ebdcbd0b210c23930894
h-peng17 <penghao20170136@163.com>
2022-07-28 15:56:42 +0800

    Add data and readme

--- build.sh
+++ build.sh
@@ -2,16 +2,14 @@
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.12.0-linux-x86_64.tar.gz ~
 tar xvzf ~/elasticsearch-7.12.0-linux-x86_64.tar.gz 
 
-pip3 install hnswlib --user 
-pip3 install Pillow --user 
-pip3 install matplotlib --user 
-pip3 install pytorch_metric_learning --user 
-pip3 install elasticsearch --user
-pip3 install nltk --user
-
+# pypi
+pip install -r requirements.txt
 
 # prepare data
-mkdir data
+wget --content-disposition https://cloud.tsinghua.edu.cn/f/8c01c331523b43088bac/?dl=1 
+unzip data.zip
+rm data.zip 
+
 
 
 
